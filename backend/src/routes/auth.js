59c2fd5b35b7e078e3f9e62877ea3fc5cad44e2c
Dmytro Mykhailenko <midmihua@users.noreply.github.com>
2020-02-18 12:35:14 +0200

    small refactoring has been done (#9)

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-// TBD: refactoring needed (implement DI pattern)
-const { isAuthenticated } = require('../middleware/isAuthenticated');
 const { validatePut, validatePost } = require('./validation/auth');
-const User = require('../models/user');
-//----------------------------------------------
-
 const { AUTH } = require('./routes');
 const { signup, login, me } = require('../controllers/auth');
 
-router.route(AUTH.ROUTES.SIGNUP).post(validatePut(User), signup);
+const initRoutes = (userModel, authMiddleware) => {
 
-router.route(AUTH.ROUTES.LOGIN).post(validatePost(), login);
+    router.route(AUTH.ROUTES.SIGNUP).post(validatePut(userModel), signup);
 
-router.route(AUTH.ROUTES.ME).get(isAuthenticated, me);
+    router.route(AUTH.ROUTES.LOGIN).post(validatePost(), login);
+    
+    router.route(AUTH.ROUTES.ME).get(authMiddleware, me);
 
-module.exports.authRoutes = router;
+    return router;
+};
+
+module.exports.authRoutes = initRoutes;
