64ff1139b1a099ab10d2dd1c45484b394e9c1ecf
Dmytro Mykhailenko <midmihua@users.noreply.github.com>
2020-04-28 17:34:29 +0300

    /PUT fixed (#42)
    Co-authored-by: Dmytro Mykhailenko <ukr-dmytromykhailenko@Dmytros-MacBook-Pro.local>

--- backend/src/middleware/errorHandling.js
+++ backend/src/middleware/errorHandling.js
@@ -1,3 +1,12 @@
+const mongoDbErrorMessage = ({codeName, keyValue}) => {
+  switch (codeName) {
+    case 'DuplicateKey':
+      return [409, `Value already exists in database: ${JSON.stringify(keyValue)}`];
+    default:
+      return [400, 'MongoError occured'];
+  }
+};
+
 module.exports.errorHandling = (err, req, res, next) => {
 
   let error = err.message;
@@ -10,14 +19,17 @@ module.exports.errorHandling = (err, req, res, next) => {
     case 'CastError':
       status = 404;
       error = 'Incorrect objectId provided';
-      break;    
+      break;
+    case 'MongoError':
+      [status, error] = mongoDbErrorMessage(err);
+      break;
   }
 
   const message = {
     error,
-    description: Array.isArray(error.validationErrors) &&
-      error.validationErrors.length > 0 ?
-      error.validationErrors : undefined
+    description: Array.isArray(err.validationErrors) &&
+    err.validationErrors.length > 0 ?
+    err.validationErrors : undefined
   };
 
   res.status(status).json(message);
